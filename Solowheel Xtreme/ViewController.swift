befdbb296704d094f38c212e6d5476f56c92f797
kroot <CEqQ8Xwg>
2015-09-24 17:16:14 -0700

    Initial Commit

---
+++ Solowheel Xtreme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Solowheel Xtreme
+//
+//  Created by kroot on 9/24/15.
+//  Copyright © 2015 Inventist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Solowheel XtremeTests/Solowheel_XtremeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Solowheel_XtremeTests.swift
+//  Solowheel XtremeTests
+//
+//  Created by kroot on 9/24/15.
+//  Copyright © 2015 Inventist. All rights reserved.
+//
+
+import XCTest
+@testable import Solowheel_Xtreme
+
+class Solowheel_XtremeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
